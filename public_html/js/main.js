b1177ae97b69b21bdfe19346595dac9af478ebef
Brandon Harrison <brandonharrisoncode@gmail.com>
2018-07-29 19:32:41 -0700

    Add animation for intro

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,22 @@
 "use strict";
+
+function portraitLoad() {
+    var intro = document.getElementById('intro')
+    var headshot = document.getElementById('headshot')
+    intro.style.visibility = 'visible';
+    addClass(intro, 'slide-to-right');
+    addClass(headshot, 'slide-to-right');
+
+    var resume = document.getElementById('resume');
+    addClass(resume, 'push-to-right')
+
+    var bio = document.getElementById('bio');
+    addClass(bio, 'grow-text');
+}
+
+function addClass(elem, className) {
+    if (elem.classList)
+        elem.classList.add(className);
+    else
+        elem.className += ' ' + className;
+}
