df030972382d80e0dfc44baca98332975fb5be8b
Brandon Harrison <brandonharrisoncode@gmail.com>
2018-07-30 20:47:29 -0700

    Add opening animation for the site

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
 ###  This script is designed to automatically deploy the website to AWS
+
+source .env
 aws s3 sync public_html s3://brandonharrisoncode.com
+aws cloudfront create-invalidation --distribution-id $CDN_DISTRIBUTION_ID --paths "/*"
--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,17 +1,38 @@
 "use strict";
 
-function portraitLoad() {
+async function portraitLoad() {
+    await sleep(2000);
+
     var intro = document.getElementById('intro')
     var headshot = document.getElementById('headshot')
     intro.style.visibility = 'visible';
     addClass(intro, 'slide-to-right');
     addClass(headshot, 'slide-to-right');
 
+    var logo = document.getElementById('logo');
     var resume = document.getElementById('resume');
+    var resumetext = document.getElementById('resumetext');
     addClass(resume, 'push-to-right')
+    addClass(logo, 'push-to-right')
 
     var bio = document.getElementById('bio');
     addClass(bio, 'grow-text');
+
+    await sleep(1000);
+
+    resume.style.width = '50vw';
+    logo.style.width = '50vw';
+
+    await sleep(500);
+
+    addClass(logo, 'fade-out');
+    await sleep(400);
+
+    resumetext.style.visibility = 'visible';
+    addClass(resumetext, 'fade-in');
+
+    await sleep(600);
+    logo.style.visibility = 'hidden';
 }
 
 function addClass(elem, className) {
@@ -19,4 +40,8 @@ function addClass(elem, className) {
         elem.classList.add(className);
     else
         elem.className += ' ' + className;
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
