06b880e3e71305cc35dfbeb8e8370b4ce2133350
Brandon Harrison <brandonharrisoncode@gmail.com>
2018-07-30 23:08:28 -0700

    Make site responsive, even after site has initialized

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,21 +1,62 @@
 "use strict";
 
+function WidthChange(screensize) {
+    var intro = document.getElementById('intro');
+    var logo = document.getElementById('logo');
+    var resume = document.getElementById('resume');
+    var headshot = document.getElementById('headshot')
+
+    if(screensize.matches) {
+        intro.style.width = '50vw';
+        resume.style.width = '50vw';
+        logo.style.width = '50vw';
+        headshot.style.width = '50vw';
+    } else {
+        intro.style.width = '100vw';
+        resume.style.width = '100vw';
+        logo.style.width = '100vw';
+        headshot.style.width = '100vw';
+    }
+}
+
+const largeScreen = window.matchMedia( "(min-device-width: 40em)" );
+largeScreen.addListener(WidthChange);
+
 async function portraitLoad() {
-    await sleep(2000);
+    if(!largeScreen.matches) {
+        var intro = document.getElementById('intro');
+        intro.style.visibility = 'visible';
+
+        var logo = document.getElementById('logo');
+        var resume = document.getElementById('resume');
+        var resumetext = document.getElementById('resumetext');
+
+        resumetext.style.visibility = 'visible';
+        logo.style.visibility = 'hidden';
+
+        return;
+    }
 
     var intro = document.getElementById('intro')
     var headshot = document.getElementById('headshot')
+    var logo = document.getElementById('logo');
+    var resume = document.getElementById('resume');
+    var resumetext = document.getElementById('resumetext');
+    var bio = document.getElementById('bio');
+
+    // Make it so there is no scroll bar during the loading screen
+    var resumetemp = resumetext.innerHTML;
+    resumetext.innerHTML = '';
+
+    await sleep(2000);
+
     intro.style.visibility = 'visible';
     addClass(intro, 'slide-to-right');
     addClass(headshot, 'slide-to-right');
 
-    var logo = document.getElementById('logo');
-    var resume = document.getElementById('resume');
-    var resumetext = document.getElementById('resumetext');
     addClass(resume, 'push-to-right')
     addClass(logo, 'push-to-right')
 
-    var bio = document.getElementById('bio');
     addClass(bio, 'grow-text');
 
     await sleep(1000);
@@ -25,6 +66,7 @@ async function portraitLoad() {
 
     await sleep(500);
 
+    resumetext.innerHTML = resumetemp;
     addClass(logo, 'fade-out');
     await sleep(400);
 
