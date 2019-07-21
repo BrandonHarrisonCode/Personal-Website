aaa49a6d9af1c1c61de10126be5640d40b6b37c4
Brandon Harrison <brandonharrisoncode@gmail.com>
2019-07-21 15:33:10 -0500

    Add cloudformation

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 ###  This script is designed to automatically deploy the website to AWS
 
 source .env
-aws s3 sync public_html s3://brandonharrisoncode.com
+aws s3 sync public_html $BUCKET_ADDRESS
 aws cloudfront create-invalidation --distribution-id $CDN_DISTRIBUTION_ID --paths "/*"
