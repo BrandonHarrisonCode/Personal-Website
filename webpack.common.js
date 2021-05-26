8eb3403210b2cc9fda5614996b2de330c57752ab
Brandon Harrison <brandonharrisoncode@gmail.com>
2021-05-26 15:05:18 -0600

    Reenable favicon

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -25,6 +26,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new FaviconsWebpackPlugin('./src/logo.png'),
     new HtmlWebpackPlugin({
       template: './src/index.html',
       title: 'Brandon Harrison Portfolio',
